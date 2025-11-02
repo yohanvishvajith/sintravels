7838ee5fc6472c4b80ba0151a4aece984a74dc53
yohan vishvajith <yohanvishvajith@gmail.com>
2025-11-02 09:55:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["flagcdn.com", "upload.wikimedia.org"],
+    domains: ["flagcdn.com", "upload.wikimedia.org", "sintravelsandmanpower.com"],
   },
 };
 
